c1ea9f7b757774931e54bf38b2bb84f1002f0d98
Robert Morgan <robert.morgan0703@gmail.com>
2022-05-03 23:56:59 +0100

    added evolutions

--- src/components/PokemonShow.js
+++ src/components/PokemonShow.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Accordion } from 'react-bootstrap'
+
 
 
 
 const PokemonShow = () => {
   const [ pokemon, setPokemon ] = useState(null)
+  const [ pokemonChain, setPokemonChain] = useState(null)
+  
+  const [ previousEvolution, setPreviousEvolution ] = useState(null)
+  const [ nextEvolution, setNextEvolution ] = useState(null)
   
   const { name } = useParams()
 
@@ -22,12 +28,57 @@ const PokemonShow = () => {
         console.log(error)
       }
     }
+
+
+    const getEvolution = async () => {
+      try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+        setPokemonChain(data)
+        try {
+          if (!data.evolves_from_species.name){
+            setPreviousEvolution(null)
+          } else {
+            setPreviousEvolution(data.evolves_from_species.name)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     getPokemon()
+    getEvolution()
   }, [])
 
+  useEffect(() => {
+    if (pokemonChain){
+      const evoData = async () => {
+        try {
+          const { data } = await axios.get(pokemonChain.evolution_chain.url)
+          try {
+            if (data.chain.evolves_to[0].evolves_to[0].species.name === name){
+              setNextEvolution(null)
+            } else if (data.chain.evolves_to[0].species.name === name) {
+              console.log(data)
+              setNextEvolution(data.chain.evolves_to[0].evolves_to[0].species.name)
+            } else {
+              setNextEvolution(data.chain.evolves_to[0].species.name)
+            }
+          } catch (error) {
+            console.log(error)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      evoData()
+    } else if (!pokemonChain){
+      console.log('notyet')
+    }
 
-
-
+  }, [pokemonChain])
 
   return (
     <div>
@@ -36,26 +87,16 @@ const PokemonShow = () => {
           <Row>
             <Col>
               <h1>{pokemon.name}</h1>
-              <h2>{pokemon.types[0].type.name}</h2>
-              <div>
-                {pokemon.moves.map((pokemon, index) => {
-                  return (
-                    <div key={index}>
-                      {pokemon.move.name}
-                    </div>
-                  )
+              <div id='show_types'>
+                {pokemon.types.map(pokemonType => {
+                  const { slot, type } = pokemonType
+                  return <div className={type.name} key={slot}><h2>{type.name}</h2></div>
                 })}
-              </div>
-            </Col>
-            <Col>
-              <h2>{pokemon.id}</h2>
-              <div className='pokemon-image'>
-                <img src={pokemon.sprites.other['official-artwork'].front_default} />
               </div>
               <div className='pokemon-stats'>
                 {pokemon.stats.map((stat, index) => {
                   return (
-                    <Container key={index}>
+                    <Container className='stats' key={index}>
                       <Row>
                         <Col>
                           <div>
@@ -69,6 +110,30 @@ const PokemonShow = () => {
                     </Container>
                   )
                 })} 
+              </div>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Moves</Accordion.Header>
+                  <Accordion.Body>
+                    {pokemon.moves.map((pokemon, index) => {
+                      return (
+                        <div key={index}>
+                          {pokemon.move.name}
+                        </div>
+                      )
+                    })}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+            <Col>
+              <div className='pokemon-image'>
+                <h2>#{pokemon.id}</h2>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} />
+              </div>
+              <div>
+                {nextEvolution && <p>Next: {nextEvolution}</p>}
+                {previousEvolution && <p>Previous: {previousEvolution}</p>}
               </div>
             </Col>
           </Row>
