842a84c1561dbb42089edc3d47195e8b23790bad
Marko Filipovic <markoravnice25@gmail.com>
2022-05-03 21:22:08 +0200

    some changes to home page styling

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
+//react-router-dom components
+import { Link } from 'react-router-dom'
+
 const Home = () => {
 
   return (
-    <div className='hero'>
+    <div className='hero text-center'>
       <section className='hero-container'>
         <div className='title'>
-          <h1>Pokedex!</h1>
-        </div>
-        <div className='hero-filters'>
-          <h1>pokemon indexes</h1>
+          <h1 className='display-1'>Pokedex!</h1> <img src="src/images/shiny-furfrou.gif" alt="" />
+          <p className='lead'>Discover all your favourite Pokeman&rsquo;s</p>
+          <Link to='/pokemons' className='btn btn-outline-danger'>Pokemon Index</Link>
         </div>
       </section>
 
